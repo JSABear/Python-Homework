5bcf8728a41f2949bdcaae47d5d03cefb5d49062
Farseer <santtu.karhu@live.fi>
2022-09-04 02:29:34 +0300

    start

---
+++ Moduuli 3.py
@@ -0,0 +1,26 @@
+#1
+#pituus = float(input("Anna kalan pituus senteissä: "))
+#if pituus < 37:
+#    print ("Kala on ", str(37 - pituus) + "cm liian lyhyt. Vapauta kala takisin veteen")
+#else:
+#    print ("Kala on tarpeeksi pitkä.")
+
+#2
+#hytti= input("Anna hytin luokka: ")
+#if hytti == LUX:
+#    print ("LUX on parvekkeellinen hytti yläkannella.")
+#if hytti == A:
+#    print ("A on ikkunallinen hytti autokannen yläpuolella.")
+#if hytti == B:
+#    print ("B on ikkunaton hytti autokannen yläpuolella.")
+#if hytti == C:
+#    print ("C on ikkunaton hytti autokannen alapuolella.")
+#else:
+#    print ("Virheellinen hyttiluokka.")
+
+#3
+#nainen=1
+#sukupuoli = input("Anna sukupuolesi: ")
+#nainen=1
+#if sukupuoli ==1:
+#    Print ("Olet nainen.")
