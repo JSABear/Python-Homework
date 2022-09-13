e3793d98364daa54d885570ede78ea40aa724c18
Karhu <santtu.karhu@live.fi>
2022-09-13 20:04:09 +0300

    korjauksia

--- Moduuli 7.1.py
+++ Moduuli 7.1.py
@@ -1,19 +1,26 @@
 
-kuukaudet = ("joulukuu","tammikuu","helmikuu","maaliskuu","huhtikuu",
+kuukaudet = ("joulkuu","tammikuu","helmikuu","maaliskuu","huhtikuu",
               "toukokuu","kesäkuu","heinäkuu","elokuu",
              "syyskuu","lokakuu","marraskuu")
-while True:
 
-    vastaus = input("Anna kuukausi: ")
 
-    if vastaus in kuukaudet[0:3]:
-        print("On talvi")
+syöttö = int(input("Anna kuukausi: "))
+if syöttö > 11:
+    muutos = syöttö-12
+elif syöttö < 12:
+    muutos = syöttö
 
-    elif vastaus in kuukaudet[3:6]:
-        print("On kevät")
+vastaus = kuukaudet[muutos]
 
-    elif vastaus in kuukaudet[6:9]:
-        print("On kesä")
 
-    elif vastaus in kuukaudet[9:12]:
-        print("On syksy")
+if vastaus in kuukaudet[0:3]:
+    print("On talvi")
+
+elif vastaus in kuukaudet[3:6]:
+    print("On kevät")
+
+elif vastaus in kuukaudet[6:9]:
+    print("On kesä")
+
+elif vastaus in kuukaudet[9:12]:
+    print("On syksy")
